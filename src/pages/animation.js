7bad645a22e183d3cc458817991736b2c24282bb
Lizik <elizavetamomsen@gmail.com>
2021-11-11 11:42:41 +0200

    24. nav line animation

--- src/pages/animation.js
+++ src/pages/animation.js
@@ -1,7 +1,7 @@
 export const pageAnimation = {
 	hidden: {
 		opacity: 0,
-		y: 300,
+		y: -300,
 	},
 	show: {
 		opacity: 1,
@@ -14,7 +14,7 @@ export const pageAnimation = {
 	},
 	exit: {
 		opacity: 0,
-		y: 300,
+		y: -300,
 		transition: {
 			duration: 0.5,
 		},
